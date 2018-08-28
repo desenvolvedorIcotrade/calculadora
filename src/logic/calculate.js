779b9ce1f8f8aa9485cd46fce533a01214620cdf
cleverTon <35335846+desenvolvedorIcotrade@users.noreply.github.com>
2018-08-28 17:35:13 -0300

    Update calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,8 +1,16 @@
 import Big from "big.js";
-
+import big fron" big.js"
 import operate from "./operate";
 import isNumber from "./isNumber";
-
+/**
+ * Given a button name and a calculator data object, return an updated
+ * calculator data object.
+ *
+ * Calculator data object contains:
+ *   total:String      the running total
+ *   next:String       the next number to be operated on with the total
+ *   operation:String  +, -, A >= 9,0 B >= 7,5 e < 9,0 C >= 6,0 e < 7,5 D >= 4,0 e < 6,0 E < 4,0  etc.
+ */
 /**
  * Given a button name and a calculator data object, return an updated
  * calculator data object.
